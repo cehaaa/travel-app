db86f20881c941550f9d2f896d9cea3def087c44
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-05 23:21:51 +0700

    feat: container component

---
+++ src/components/Basics/Container/Container.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="container">
+		<slot />
+	</div>
+</template>
+
+<style lang="scss">
+.container {
+	padding: 0 20px;
+	width: 100%;
+}
+</style>

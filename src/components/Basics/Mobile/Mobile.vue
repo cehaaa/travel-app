344d5c0f8344865f80b414ef2ffd37c04a6a308d
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-05 23:21:39 +0700

    feat: mobile component

---
+++ src/components/Basics/Mobile/Mobile.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+	<main class="mobile">
+		<slot />
+	</main>
+</template>
+
+<style scoped lang="scss">
+@import "./../../../scss/_variables.scss";
+.mobile {
+	width: 390px;
+	min-height: 100vh;
+	margin: 0 auto;
+	background: $gray-50;
+	overflow: hidden;
+	position: relative;
+}
+</style>

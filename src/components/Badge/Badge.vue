a947604640b8dfcac00c88c87ea72c8448680aec
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-07 20:43:45 +0700

    feat: badge component

---
+++ src/components/Badge/Badge.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup></script>
+
+<template>
+	<div class="badge">
+		<slot />
+	</div>
+</template>
+
+<style lang="scss" scoped>
+@import "./../../scss/_variables.scss";
+.badge {
+	position: absolute;
+	top: 15px;
+	right: 15px;
+	background: rgba(107, 114, 128, 0.7);
+	padding: 5px 12px;
+	color: white;
+	font-size: $sm;
+	font-weight: 500;
+	border-radius: 5px;
+}
+</style>

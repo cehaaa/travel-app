42bce534163127706766aa4441c29a8531e4dc55
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-07 20:51:11 +0700

    feat: footer component

---
+++ src/components/Basics/Footer/Footer.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import Icon from "./../Icons/Icons";
+</script>
+
+<template>
+	<footer class="footer">
+		<div>
+			<Icon.Home />
+		</div>
+		<div>
+			<Icon.Plus />
+		</div>
+		<div>
+			<Icon.User />
+		</div>
+	</footer>
+</template>
+
+<style lang="scss" scoped>
+.footer {
+	width: 390px;
+	background: white;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	position: fixed;
+	bottom: 0;
+	height: 72px;
+	padding: 0 50px;
+}
+</style>

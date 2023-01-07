156f8e7b5fe1b141fdd830712e91d3403dc7f362
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-07 19:59:06 +0700

    feat: icon component

---
+++ src/components/Basics/Icons/ElipsisVerticalIcon.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+interface ElipsisVerticalIconProps {
+	class: string;
+}
+
+const props = defineProps<ElipsisVerticalIconProps>();
+</script>
+
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		:class="{ 'card-icon': props.class }"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+		/>
+	</svg>
+</template>
---
+++ src/components/Basics/Icons/Icons.ts
@@ -0,0 +1,15 @@
+import HomeIcon from "./HomeIcon.vue";
+import PlusIcon from "./PlusIcon.vue";
+import UserIcon from "./UserIcon.vue";
+import ElipsisVerticalIcon from "./ElipsisVerticalIcon.vue";
+import BookMarkIcon from "./BookMarkIcon.vue";
+
+const Icon = {
+	Home: HomeIcon,
+	Plus: PlusIcon,
+	User: UserIcon,
+	ElipsisVertical: ElipsisVerticalIcon,
+	Bookmark: BookMarkIcon,
+};
+
+export default Icon;

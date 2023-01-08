10ebcebc1ed18db367c9949a904ebf1f71ebb552
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-08 12:55:26 +0700

    feat: guide interface

--- src/Interfaces/Guide.ts
+++ src/Interfaces/Guide.ts
@@ -1,8 +1,10 @@
 import Person from "./Person";
+import Plan from "./Plan";
 
 export default interface Guide {
 	title: string;
 	description: string;
 	src: string;
 	publisher: Person;
+	plans?: Array<Array<Plan>>;
 }

24d3a70df629c6d14b652d85d2576746141ad859
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-08 13:35:46 +0700

    style: fix mobil style

--- src/components/Basics/Mobile/Mobile.vue
+++ src/components/Basics/Mobile/Mobile.vue
@@ -9,11 +9,11 @@
 <style scoped lang="scss">
 @import "./../../../scss/_variables.scss";
 .mobile {
+	padding-bottom: 90px;
 	width: 390px;
 	min-height: 100vh;
 	margin: 0 auto;
 	background: $gray-50;
 	overflow: hidden;
-	position: relative;
 }
 </style>

808b0cafb82e1d6374a2f521b6b350cbe35ed023
Christian Gouw <55861341+veeloo@users.noreply.github.com>
2023-01-09 22:48:46 +0700

    feat: button component

---
+++ src/components/Basics/Button/Button.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+interface ButtonProps {
+	variant?: string;
+}
+
+defineProps<ButtonProps>();
+</script>
+
+<template>
+	<button :class="{ 'w-full': variant === 'full' }">
+		<slot />
+	</button>
+</template>
+
+<style lang="scss" scoped>
+@import "./../../../scss/_variables.scss";
+
+button {
+	background: $primary;
+	color: white;
+	font-size: $sm;
+	font-weight: 500;
+	border: none;
+	border-radius: 10px;
+	padding: 15px 35px;
+	cursor: pointer;
+
+	&.w-full {
+		width: 100%;
+	}
+}
+</style>
